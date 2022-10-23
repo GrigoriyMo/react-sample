c46511f967fceacea863119fd426305940e531ca
GrigoriyMo <kovarscky.g@yandex.ru>
2022-10-23 23:06:22 +0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   const [count, setCount] = useState(0)
   const [magic, setMagic] = useState([])
 
+  componentDidMount({})
+
   const updateMagic = () => {
     setMagic(arr => [...arr, `${arr.length}`]);
   }
